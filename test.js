7cc71f42898a360bce7d9aa01ee36f05872eb635
Ben Conway <benrconway84@gmail.com>
2018-03-29 13:20:41 +0100

    Refactored to provide clearer answers

--- test.js
+++ test.js
@@ -1,29 +1,38 @@
-//During the test the env variable is set to test
+// During the test the env variable is set to test
+// This part presumes a config that is not present in this file. A way to make
+// your tests use a DB that isn't the one you use for production etc.
 process.env.NODE_ENV = 'test';
 
-//Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = "0.0.0.0:3000";
-let should = chai.should();
+// Require the dev-dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = "localhost:3000";
+const should = chai.should();
 
+// Here we tell Chai to use the Chai-HTTP package.
 chai.use(chaiHttp);
-//Our parent block
-describe('API test', () => {
-/*
-  * Test the /GET route
-  */
-  describe('/GET info', () => {
-      it('it should GET all the info', (done) => {
+
+// Our parent describe block
+describe('API functionality test', () => {
+  // Possible hooks are before, beforeEach, afterEach and after.
+  // * Example beforeEach *
+  //    beforeEach((done) => {
+  //       function-to-perform({}, (err) => {
+  //         done();
+  //       });
+  //     });
+
+// Child describe block testing the /api GET route
+  describe('testing /api GET route', () => {
+      it('it should GET appropriate information', (done) => {
         chai.request(server)
             .get('/api')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.be.eql({name: "John"});
+            .end((error, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+                response.body.should.be.eql({name: "John"});
               done();
             });
-      });
-  });
-
+        });
+    });
 });
